93939dbde09d6022c9de19ff3a9b0e4765f76b0e
xialiangliang <xiayunfeng2012@gmail.com>
2017-12-14 10:17:39 +0800

    update

--- app/src/main/java/com/yunfeng/tools/phoneproxy/MainActivity.java
+++ app/src/main/java/com/yunfeng/tools/phoneproxy/MainActivity.java
@@ -8,30 +8,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 import com.yunfeng.tools.phoneproxy.http.HttpsServer;
 import com.yunfeng.tools.phoneproxy.socket.Cert;
+import com.yunfeng.tools.phoneproxy.util.Utils;
 
-import org.bouncycastle.util.encoders.Base64Encoder;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.security.Key;
-import java.security.KeyPair;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.cert.Certificate;
-import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,14 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_PERMISIONS = 0xffffff;
 
-    private static final String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final String[] permissions = new String[]{Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        showWelDialog(this);
 
         tv = (TextView) findViewById(R.id.proxy_info);
 
@@ -60,140 +42,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    private void showWelDialog(final Activity activity) {
-//        final Dialog channleWelDialog = new Dialog(activity, R.style.Channel_CustomDialog);
-//        channleWelDialog.setContentView(R.layout.channel_welcome);
-//        final ImageView view = (ImageView) channleWelDialog.findViewById(R.id.image_view);
-//        final Handler mHandler = new Handler();
-//        Animation.AnimationListener firstAnimationListener = new Animation.AnimationListener() {
-//            @Override
-//            public void onAnimationStart(Animation arg0) {
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation arg0) {
-//            }
-//
-//            @Override
-//            public void onAnimationEnd(Animation arg0) {
-//                view.setBackgroundDrawable(null);
-//                mHandler.postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        view.setBackgroundResource(R.drawable.second);
-//                        Animation.AnimationListener thirdAnimationListener = new Animation.AnimationListener() {
-//                            @Override
-//                            public void onAnimationStart(Animation arg0) {
-//                            }
-//
-//                            @Override
-//                            public void onAnimationRepeat(Animation arg0) {
-//                            }
-//
-//                            @Override
-//                            public void onAnimationEnd(Animation arg0) {
-//                                mHandler.postDelayed(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                        channleWelDialog.dismiss();
-//                                        view.setBackgroundDrawable(null);
-//                                        System.gc();
-//                                    }
-//                                }, 3000);
-//                            }
-//                        };
-//                        splashAnimation(view, thirdAnimationListener);
-//                    }
-//                }, 3000);
-//            }
-//        };
-//        splashAnimation(view, firstAnimationListener);
-//        Drawable drawable = activity.getResources().getDrawable(bg_id);
-//        view.setBackgroundDrawable(drawable);
-//        channleWelDialog.show();
-//        final Timer tTimer = new Timer();
-//        tTimer.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                activity.runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        view.setBackgroundDrawable(null);
-//                        view.setBackgroundResource(R.drawable.second);
-//                        tTimer.schedule(new TimerTask() {
-//                            @Override
-//                            public void run() {
-//                                activity.runOnUiThread(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                        view.setBackgroundDrawable(null);
-//                                        channleWelDialog.dismiss();
-//                                    }
-//                                });
-//                                tTimer.cancel();
-//                            }
-//                        }, 3000);
-//                    }
-//                });
-//            }
-//        }, 3000);
-//
-//    }
-
-    private void splashAnimation(View view, Animation.AnimationListener animationListener) {
-        ScaleAnimation scaleAnimation = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        scaleAnimation.setDuration(600);
-        scaleAnimation.setAnimationListener(animationListener);
-        view.startAnimation(scaleAnimation);
-    }
-
     @Override
     @TargetApi(Build.VERSION_CODES.M)
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_PERMISIONS:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("PERMISSION_GRANTED is true!");
-                    export("BKS", "clientkey", "a123456".toCharArray(), "/sdcard/exported.crt");
+
                 } else {
                     requestPermissions(permissions, REQUEST_PERMISIONS);
                 }
                 break;
-        }
-    }
-
-    public KeyPair getPrivateKey(KeyStore keystore, String alias, char[] password) {
-        try {
-            Key key = keystore.getKey(alias, password);
-            if (key instanceof PrivateKey) {
-                Certificate cert = keystore.getCertificate(alias);
-                PublicKey publicKey = cert.getPublicKey();
-                return new KeyPair(publicKey, (PrivateKey) key);
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public void export(String type, String alias, char[] password,
-                       String outFilePath) {
-        try {
-            KeyStore keystore = KeyStore.getInstance(type);
-            InputStream in = this.getAssets().open("bksmy.keystore");
-            keystore.load(in, password);
-            KeyPair keyPair = getPrivateKey(keystore, alias, password);
-            PrivateKey privateKey = keyPair.getPrivate();
-            FileOutputStream writer = new FileOutputStream(
-                    new File(outFilePath));
-            Base64Encoder encoder = new Base64Encoder();
-            int encoded = encoder.encode(privateKey.getEncoded(), 0,
-                    privateKey.getEncoded().length, writer);
-            System.out.println("size:" + encoded);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         }
     }
 
@@ -225,27 +84,8 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            tv.setText(getLocalIpAddress() + ":8888");
+            tv.setText(Utils.getLocalIpAddress() + ":8888");
         }
-    }
-
-    public String getLocalIpAddress() {
-        try {
-            Enumeration<NetworkInterface> ens = NetworkInterface.getNetworkInterfaces();
-            while (ens.hasMoreElements()) {
-                NetworkInterface networkInterface = ens.nextElement();
-                Enumeration<InetAddress> ias = networkInterface.getInetAddresses();
-                while (ias.hasMoreElements()) {
-                    InetAddress inetAddress = ias.nextElement();
-                    if (!inetAddress.isLoopbackAddress() && (inetAddress instanceof Inet4Address)) {
-                        return inetAddress.getHostAddress().toString();
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
 }
