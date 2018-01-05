c1b6e4c0dc01e50cd92b2836ebaf2f32d361e830
xialiangliang <xiayunfeng2012@gmail.com>
2018-01-05 18:03:53 +0800

    update

--- app/src/main/java/com/yunfeng/tools/phoneproxy/MainActivity.java
+++ app/src/main/java/com/yunfeng/tools/phoneproxy/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdListener;
@@ -17,11 +19,20 @@ import com.google.android.gms.ads.MobileAds;
 import com.yunfeng.tools.phoneproxy.http.SocketProxy;
 import com.yunfeng.tools.phoneproxy.util.Logger;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_PERMISIONS = 0xffffff;
 
     private static final String[] permissions = new String[]{Manifest.permission.INTERNET};
+    private final List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +78,26 @@ public class MainActivity extends AppCompatActivity {
                 Logger.d("onAdClosed");
             }
         });
+        ListView listView = (ListView) findViewById(R.id.list_ips);
 
+		/*SimpleAdapter的参数说明
+         * 第一个参数 表示访问整个android应用程序接口，基本上所有的组件都需要
+		 * 第二个参数表示生成一个Map(String ,Object)列表选项
+		 * 第三个参数表示界面布局的id  表示该文件作为列表项的组件
+		 * 第四个参数表示该Map对象的哪些key对应value来生成列表项
+		 * 第五个参数表示来填充的组件 Map对象key对应的资源一依次填充组件 顺序有对应关系
+		 * 注意的是map对象可以key可以找不到 但组件的必须要有资源填充  因为 找不到key也会返回null 其实就相当于给了一个null资源
+		 * 下面的程序中如果 new String[] { "name", "head", "desc","name" } new int[] {R.id.name,R.id.head,R.id.desc,R.id.head}
+		 * 这个head的组件会被name资源覆盖
+		 * */
+        final SimpleAdapter simplead = new SimpleAdapter(this, listems,
+                R.layout.network_list, new String[]{"name", "head", "desc"},
+                new int[]{R.id.name, R.id.head, R.id.desc});
+        listView.setAdapter(simplead);
+        listView.setItemsCanFocus(false);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        addListData();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             for (String permission : permissions) {
                 int granted = checkSelfPermission(permission);
@@ -90,6 +120,34 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    private void addListData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+                    while (nis.hasMoreElements()) {
+                        NetworkInterface ni = nis.nextElement();
+                        Enumeration<InetAddress> ias = ni.getInetAddresses();
+                        while (ias.hasMoreElements()) {
+                            InetAddress ia = ias.nextElement();
+                            String addr = ia.getHostAddress();
+                            String hostName = ia.getHostName();
+                            Logger.d("addr: " + addr + ", hostName: " + hostName);
+                            Map<String, Object> listem = new HashMap<String, Object>();
+                            listem.put("head", addr);
+                            listem.put("name", hostName);
+                            listem.put("desc", ia.getCanonicalHostName());
+                            listems.add(listem);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     public void startProxy(View view) {
